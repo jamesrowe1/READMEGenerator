89feb5b9a04945226770258d50a23965a5fd434b
James Rowe <jamesrowe@outlook.com>
2020-07-22 16:00:42 -0400

    the whole darn thing

--- 02-Homework/index.js
+++ 02-Homework/index.js
@@ -1,15 +1,86 @@
+//requires fs and inquirer
+var fs = require("fs");
+var inquirer = require("inquirer");
+var generateMarkdown = require("./utils/generateMarkdown.js");
+
+const { title } = require("process");
+
 // array of questions for user
 const questions = [
+  { name: "title", message: "Project Title:", type: "input" },
+  { name: "description", message: "Enter a Description:", type: "input" },
+  {
+    name: "installationInstructions",
+    message: "Enter Installation Instructions:",
+    type: "input",
+  },
+  {
+    name: "usageInformation",
+    message: "Enter Usage Information:",
+    type: "input",
+  },
+  {
+    name: "contributionGuidelines",
+    message: "Enter Contribution Guidelines:",
+    type: "input",
+  },
+  {
+    name: "testInstructions",
+    message: "Enter Test Instructions:",
+    type: "input",
+  },
+  {
+    name: "license",
+    message: "Choose License:",
+    type: "list",
+    choices: ["MIT", "Mozilla"],
+  },
+  {
+    name: "githubUsername",
+    message: "Enter Github Username:",
+    type: "input",
+  },
+  {
+    name: "emailAddress",
+    message: "Enter Email Address:",
+    type: "input",
+  },
+];
 
+let licenseArr = [
+  {
+    licenseName: "MIT",
+    link:
+      "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)",
+  },
+  {
+    licenseName: "Mozilla",
+    link:
+      "![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)",
+  },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
 }
 
 // function to initialize program
 function init() {
+  inquirer.prompt(questions).then((answers) => {
+    var licenseLogoLink = licenseArr.filter(
+      (logo) => logo.licenseName === answers.license
+    );
+    answers.logoLink = licenseLogoLink[0].link;
+    console.log(answers);
+    let readmeText = generateMarkdown(answers);
 
+    writeToFile("README.md", readmeText);
+  });
 }
 
 // function call to initialize program
--- 02-Homework/utils/generateMarkdown.js
+++ 02-Homework/utils/generateMarkdown.js
@@ -2,6 +2,28 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+   ${data.description}
+
+  ## Installation Instructions
+  ${data.installationInstructions}
+
+  ## Usage Information
+  ${data.usageInformation}
+
+  ## Contribution Guidelines
+  ${data.contributionGuidelines}
+
+  ## Test Instructions
+  ${data.testInstructions}
+
+  ## License
+  ${data.logoLink}
+
+  ## Questions
+  Github Username: ${data.githubUsername}
+  
+  Email: ${data.emailAddress}
 `;
 }
 
